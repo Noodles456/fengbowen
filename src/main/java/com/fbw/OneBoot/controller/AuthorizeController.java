7faa1cfe495c62f32687eed3d06acb0d703f8acf
Noodles456 <136070160@qq.com>
2020-04-24 11:18:52 +0800

    不明

--- src/main/java/com/fbw/OneBoot/controller/AuthorizeController.java
+++ src/main/java/com/fbw/OneBoot/controller/AuthorizeController.java
@@ -36,38 +36,38 @@ public class AuthorizeController {
     public String callBack(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state ,
                            HttpServletResponse response){
-       AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
-       accessTokenDTO.setCode(code);
-       accessTokenDTO.setClient_id(clientId);
-       accessTokenDTO.setRedirect_uri(redirectUri);
-       accessTokenDTO.setState(state);
-       accessTokenDTO.setClient_secret(clientSecret);
+        AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = gitHubProvider.getUser(accessToken);
-       if(gitHubUser!=null && gitHubUser.getId()!=null){
-           User user=new User();
-           String token=UUID.randomUUID().toString();
-           user.setToken(token);
-           user.setName(gitHubUser.getName());
-           user.setAccountId(String.valueOf(gitHubUser.getId()));
+        if(gitHubUser!=null && gitHubUser.getId()!=null){
+            User user=new User();
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(gitHubUser.getName());
+            user.setAccountId(String.valueOf(gitHubUser.getId()));
 
-           user.setAvatarUrl(gitHubUser.getAvatarUrl());
-           userService.createUpdate(user);
+            user.setAvatarUrl(gitHubUser.getAvatarUrl());
+            userService.createUpdate(user);
 
-           response.addCookie(new Cookie("token",token));
-       return "redirect:/";
-       }else{
-           log.error("callback get gitHub error,{}",gitHubUser);
-           return "redirect:/";
-       }
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else{
+            log.error("callback get gitHub error,{}",gitHubUser);
+            return "redirect:/";
+        }
 
     }
     @GetMapping("/logout")
     public String logout(HttpServletRequest request,
                          HttpServletResponse response){
         request.getSession().removeAttribute("user");
-     Cookie cookie=new Cookie("token",null);
-     cookie.setMaxAge(0);
+        Cookie cookie=new Cookie("token",null);
+        cookie.setMaxAge(0);
         response.addCookie(cookie);
         return "redirect:/";
     }
